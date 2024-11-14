15bee87f08932da8d5b7ec1acbb47757b953eef4
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-11-14 11:07:06 -0300

    [exercicios] revisão de código

--- exercicios-assembly/3-memoria/ex-03.asm
+++ exercicios-assembly/3-memoria/ex-03.asm
@@ -1,21 +1,26 @@
 	addi t0, zero, 1
 	addi t1, zero, 1
 	addi t4, zero, vetor
+
 	sw t4, t0, 0
 	sw t4, t1, 4
-	addi t2, zero, 2
-loop:
-	bge t2, a0, fim_loop
-	add t3, t0, t1
-	addi t0, t1, 0
-	addi t1, t3, 0
 
-	slli t4, t2, 2
+	addi t2, zero, 2
+
+loop:
+	beq t2, a0, fim_loop
+
+	add t5, t0, t1
+	addi t0, t1, 0
+	addi t1, t5, 0
+
+	sll t4, t2, 2
 	addi t4, t4, vetor
-	sw t4, t3, 0
+	sw t4, t5, 0
 
 	addi t2, t2, 1
-	jal zero, loop
+	j loop
+
 fim_loop:
-	ebreak
+	break
 vetor:
