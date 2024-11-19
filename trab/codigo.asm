720d13cc375a704dec3f4f7dbe18ab50fd41fcef
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-11-19 09:59:28 -0300

    [trab] corrigir processador, instruções e sinaid de controle

--- trab/codigo.asm
+++ trab/codigo.asm
@@ -1,4 +1,4 @@
-	li r1, 0   ; N - numero de termos
+	li r1, 10   ; N - numero de termos
 	li r2, 7   ; a - termo inicial
 	li r3, 18  ; d - razao
 	li r4, 0   ; soma - soma da PA 
@@ -6,8 +6,8 @@
 	li r5, 0   ; contador
 loop:
 	branch r5, r1, fim_loop
-	mult r6, r1, r3
-	add r6, r2, r6
+	mult r6, r5, r3
+	add r6, r6, r2
 	add r4, r4, r6
 	show r4
 	
